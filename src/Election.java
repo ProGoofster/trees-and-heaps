ea32cc11b37c224a8eee9a5f99327b4b45070039
Jake Berryman <dragons4me@comcast.net>
2025-04-19 01:08:52 -0500

    init

---
+++ src/Election.java
@@ -0,0 +1,5 @@
+public class Election {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/ElectionSystem.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+
+public class ElectionSystem {
+    //initialize the election system with the list
+    //of candidates given
+    void initializeCandidates(LinkedList<String> candidates) {
+
+    }
+
+    //Simulate a vote for the specified candidate and update the priority
+    //queue
+    void castVote(String candidate) {
+
+    }
+
+    //Simulate a vote for a random candidate and update the priority queue.
+    void castRandomVote() {
+
+    }
+
+    //Simulate enough votes for the given candidate to win the
+    //election and update the priority queue. (Note: The total number of votes should be p)
+    void rigElection(String candidate){
+
+    }
+
+    //return the top k candidates with the most votes.
+    void getTopKCandidates(int k){
+
+    }
+
+    //Print to console all the candidates and how many votes they got in order from
+    //the candidate with the most votes to the candidate with the least amount of votes.
+    void auditElection(){
+
+    }
+}
